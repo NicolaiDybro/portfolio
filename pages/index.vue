6305ea40fce4412b12ed4e032f3a273a0faa7ef6
NicolaiDybro <nicolai160g@gmail.com>
2024-02-26 16:03:46 +0100

    Fixed title error

--- pages/index.vue
+++ pages/index.vue
@@ -8,14 +8,7 @@ useHead({
     });
 </script>
 
-<script setup lang="ts">
-useHead({
-  title: 'Nicolai Dybro | Portfolio',
-  meta: [
-    { name: 'description', content: 'Nicolai Dybro Portfolio.' }
-  ],
-})
-</script>
+
 
 <template>
     <ContainersContainer>
